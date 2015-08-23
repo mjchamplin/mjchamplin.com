27ebf5f30e9bdc13d5d932c4f7cd24c23d807a06
Michael Champlin <mchamplin@gmail.com>
2015-08-23 10:05:08 -0500

    assets

---
+++ assets/js/functions.js
@@ -0,0 +1,44 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
+// remap jQuery to $
+(function($){
+
+	/* trigger when page is ready */
+	$(document).ready(function (){
+	
+
+	
+	});
+	
+	
+
+	
+	$(window).load(function() {
+		
+		var container = document.querySelector('#brands');
+		var msnry = new Masonry( container, {
+		  // options
+  			columnWidth: 100,
+  			itemSelector: '.brand',
+  			gutter: 0,
+  			isFitWidth: true
+
+		});
+
+	});
+		/* optional triggers
+	$(window).resize(function() {
+		
+	});
+	
+	*/
+
+})(window.jQuery);
