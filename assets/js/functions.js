dfcf8e062d76cdc35fde944f29da152da4d00e4e
Michael Champlin <mjchamplin@gmail.com>
2015-12-02 12:46:42 -0500

    initial commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,8 +16,37 @@
  /* $('.your-class').slick({
     setting-name: setting-value
   });*/
-	$('.portfolio-flashflood').slick();
+	$('.portfolio-flashflood').slick({
+		centerMode: true,
+		centerPadding: '60px',
+	});
 
+	$('.center').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	});		
+		
 });
 	
 	
